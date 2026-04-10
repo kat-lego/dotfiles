4565de50f0bb47521dbe84a4f8696c9dd0095c67
katlego <katlegomodupi@protonmail.com>
2026-04-10 21:46:56 +0200

    update the stow configs

--- .zshrc
+++ .zshrc
@@ -132,3 +132,22 @@ export PATH="$PATH:/home/katlego/.dotnet/tools"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/katlego/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/katlego/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/katlego/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/katlego/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
+# Added by get-aspire-cli.sh
+export PATH="$HOME/.aspire/bin:$PATH"
--- init.sh
+++ init.sh
@@ -1,141 +1,131 @@
 #!/bin/bash
 
-echo "#############################"
-echo "# APT Installing some stuff #"
-echo "#############################"
+# --- Simple Logging Helpers ---
+log_header() { echo -e "\n\033[1;35m>>> $1 <<<\033[0m"; }
+log_step()   { echo -e "\033[0;34m[PROCESS]\033[0m $1"; }
+log_ok()     { echo -e "\033[0;32m[SUCCESS]\033[0m $1"; }
+log_skip()   { echo -e "\033[0;33m[SKIPPING]\033[0m $1"; }
 
-sudo apt install -y \
-    zsh \
-    git \
-    stow \
-    tmux \
-    curl \
-    wget \
-    xclip \
-    autojump \
-    ripgrep \
-    unzip \
-    gcc \
-    hugo \
+# 1. General Software
+log_header "Installing General Software"
+sudo apt install -y zsh git stow tmux curl wget xclip autojump ripgrep unzip gcc dotnet-sdk-10.0
+log_ok "Base packages installed."
 
-echo ""
-
-
-echo "#############"
-echo "# Gitconfig #"
-echo "#############"
-
+# 2. Git Config
+log_header "Setting up Git Config"
 if [ ! -f ~/.gitconfig ]; then
     cp ./.gitconfig.template ~/.gitconfig
+    log_step "Template copied."
+
+    read -p "Enter full name: " git_name
+    read -p "Enter email: " git_email
+
+    git config --global user.name "$git_name"
+    git config --global user.email "$git_email"
+    log_ok "Git identity set."
 else
-    echo ".gitconfig file already setup"
+    log_skip ".gitconfig already exists."
 fi
-echo ""
 
-
-echo "###############"
-echo "# Install TPM #"
-echo "###############"
-
+# 3. TMUX Plugin Manager
+log_header "Installing TPM"
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 if [ ! -d "$TPM_DIR" ]; then
     git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+    log_ok "TPM cloned."
 else
-    echo "$TPM_DIR directory is not empty"
+    log_skip "TPM already present."
 fi
-echo ""
 
-
-echo "################"
-echo "# Install nvim #"
-echo "################"
-
-NVIM_RELEASE_URL="https://github.com/neovim/neovim/releases/download/v0.11.3/nvim-linux-x86_64.tar.gz"
-TEMP_DIR=$(mktemp -d)
-TAR_FILE="$TEMP_DIR/nvim.tar.gz"
-EXTRACT_DIR="$TEMP_DIR/nvim-linux64"
-INSTALL_DIR="$HOME/programs"
-NVIM_DIR="$INSTALL_DIR/nvim"
-
+# 4. Neovim
+log_header "Installing Neovim"
+NVIM_DIR="$HOME/programs/nvim"
 if [ ! -d "$NVIM_DIR" ]; then
-
-   echo "Downloading Neovim..."
-   wget "$NVIM_RELEASE_URL" -O "$TAR_FILE" -nv
-
-   echo "Extracting the file..."
-   mkdir -p "$EXTRACT_DIR"
-   tar -xzf "$TAR_FILE" -C "$EXTRACT_DIR"
-
-   echo "Install to /home/katlego/programs"
-   mkdir -p $INSTALL_DIR
-   mv "$EXTRACT_DIR/nvim-linux-x86_64" "$INSTALL_DIR/nvim"
-
-   echo "Cleaning up..."
-   rm -rf "$TEMP_DIR"
+    log_step "Downloading Neovim v0.12.1..."
+    TEMP_DIR=$(mktemp -d)
+    wget "https://github.com/neovim/neovim/releases/download/v0.12.1/nvim-linux-x86_64.tar.gz" -O "$TEMP_DIR/nvim.tar.gz" -nv
+    
+    mkdir -p "$HOME/programs"
+    tar -xzf "$TEMP_DIR/nvim.tar.gz" -C "$TEMP_DIR"
+    mv "$TEMP_DIR/nvim-linux-x86_64" "$NVIM_DIR"
+    rm -rf "$TEMP_DIR"
+    log_ok "Neovim installed to $NVIM_DIR"
 else
-    echo "nvim dir already exists"
+    log_skip "Neovim already exists."
 fi
 
-echo ""
+# 5. Stow
+log_header "Running Stow"
+stow . && log_ok "Dotfiles stowed."
 
-
-echo "############"
-echo "# Run stow #"
-echo "############"
-
-stow .
-echo "stowed"
-echo ""
-
-
-echo "###########"
-echo "# go lang #"
-echo "###########"
-
-if command -v go &> /dev/null
-then
-    echo "Go is installed."
-    go version
+# 6. Go Lang
+log_header "Checking Go Lang"
+if command -v go &> /dev/null; then
+    log_skip "Go is already installed: $(go version)"
 else
-    GO_LANG_URL="https://go.dev/dl/go1.23.1.linux-amd64.tar.gz"
-    GO_TEMP_DIR=$(mktemp -d)
-    GO_TAR_FILE="$TEMP_DIR/go.tar.gz"
-
-    echo "Downloading golang..."
-    curl -L -o "$GO_TAR_FILE" "$GO_LANG_URL"
-
-    echo "Installing go"
+    log_step "Installing Go 1.23.1..."
+    GO_TEMP=$(mktemp -d)
+    curl -L "https://go.dev/dl/go1.23.1.linux-amd64.tar.gz" -o "$GO_TEMP/go.tar.gz"
     sudo rm -rf /usr/local/go
-    sudo tar -C /usr/local -xzf $GO_TAR_FILE 
-
-    echo "Cleaning up..."
-    rm -rf "$GO_TEMP_DIR"
+    sudo tar -C /usr/local -xzf "$GO_TEMP/go.tar.gz"
+    rm -rf "$GO_TEMP"
+    log_ok "Go installed."
 fi
 
-echo ""
+# 7. LazyGit
+log_header "Installing LazyGit"
+go install github.com/jesseduffield/lazygit@latest && log_ok "LazyGit binary installed via Go."
 
+# 8. Oh My Zsh
+log_header "Installing Oh My Zsh"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    log_ok "Oh My Zsh ready."
+else
+    log_skip "Oh My Zsh already exists."
+fi
 
-echo "###################"
-echo "# install lazygit #"
-echo "###################"
+# 9. NVM & Node
+log_header "Installing NVM & Node"
+if [ ! -d "$HOME/.nvm" ]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+    # Export so we can use it immediately in this script
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install 24
+    log_ok "Node 24 installed."
+else
+    log_skip "NVM already installed."
+fi
 
-go install github.com/jesseduffield/lazygit@latest
+# 10. docker
+log_header "Checking docker"
+if command -v docker &> /dev/null; then
+    log_skip "Docker is already installed"
+else
+    log_step "setup docker apt repository"
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo ""
+    (
+      echo "Types: deb"
+      echo "URIs: https://download.docker.com/linux/ubuntu"
+      echo "Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")"
+      echo "Components: stable"
+      echo "Architectures: $(dpkg --print-architecture)"
+      echo "Signed-By: /etc/apt/keyrings/docker.asc"
+    ) | sudo tee /etc/apt/sources.list.d/docker.sources
 
+    sudo apt update
 
-echo "#############"
-echo "# Oh my zsh #"
-echo "#############"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo ""
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    newgrp docker
 
-echo "#############################"
-echo "# install node 20 using nvm #"
-echo "#############################"
+    log_step "docker installed."
+fi
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-nvm install 20
-
-echo ""
+echo -e "\n\033[1;32mDONE!\033[0m Everything is set up."
